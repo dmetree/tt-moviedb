8f4ef48d61311b427c22a16b97bf280c145bac29
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-05-05 16:51:11 +0200

    Updates

---
+++ precache-manifest.2da7a188f844968df3fd77585125dfa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ce952b490edae25add849b7ebc43b0",
+    "url": "/tt-moviedb/index.html"
+  },
+  {
+    "revision": "bdc67af256688815b30b",
+    "url": "/tt-moviedb/static/css/main.ae075530.chunk.css"
+  },
+  {
+    "revision": "49d15c697b93c7e675be",
+    "url": "/tt-moviedb/static/js/2.e4f02fa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tt-moviedb/static/js/2.e4f02fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc67af256688815b30b",
+    "url": "/tt-moviedb/static/js/main.e26bcf60.chunk.js"
+  },
+  {
+    "revision": "e40651110cad96d79fc3",
+    "url": "/tt-moviedb/static/js/runtime-main.e8ba9139.js"
+  }
+]);
